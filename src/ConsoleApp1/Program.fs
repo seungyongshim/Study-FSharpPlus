57c4dab75b7583a727f7b667631df69d0fa1b22d
SeungYongShim <syshim@me.com>
2022-07-04 22:45:50 +0900

    .

--- src/ConsoleApp1/Program.fs
+++ src/ConsoleApp1/Program.fs
@@ -1,51 +1,81 @@
 open FSharpPlus
-open FSharpPlus.Data
 open FSharpPlus.Lens
 
 type Cell =
-  | Covered of Cell
-  | Number of int
-  | Bomb
-  override this.ToString() =
-    match this with
-    | Covered n -> "."
-    | Number n -> n |> string
-    | Bomb -> "*"
+   | Covered of Cell
+   | Number of int
+   | Bomb
 
-  member this.Click =
-    match this with
-    | Covered x -> x
-    | _ -> this
+   override this.ToString() =
+      match this with
+      | Covered n -> "."
+      | Number n -> n |> string
+      | Bomb -> "*"
+
+   member this.Click =
+      match this with
+      | Covered x -> x
+      | _ -> this
+
+type Width = Width of int
+type Height = Height of int
+type Column = Column of int
+type Row = Row of int
+type BombsPos = BombsPos of seq<(Row * Column)>
 
 type Minefield =
-  | Setup of int * int
-  | Playing of Map<(int * int), Cell>
-  override this.ToString() =
-    match this with
-    | Setup _ -> "Setup"
-    | Playing m ->
-      let group = m |> Map.toSeq |> groupBy (fun ((x, _), _) -> x)
+   | Setup of Width * Height * BombsPos
+   | Playing of Map<(Row * Column), Cell>
 
-      seq {
-        for (_, x) in group do
-          for (_, y) in x do
-            yield $"{y}"
+   member this.ToString1() =
+      match this with
+      | Setup _ -> "Setup"
+      | Playing m ->
+         let group = m |> Map.toSeq |> groupBy (fun ((x, _), _) -> x)
 
-          yield "\n"
-      }
-      |> fold (+) ""
+         seq {
+            for (_, x) in group do
+               for (_, y) in x do
+                  yield $"{y}"
+
+               yield "\n"
+         }
+         |> fold (+) ""
+
+   member this.Start =
+      match this with
+      | Setup (Width w, Height h, BombsPos bombsPos) ->
+         let init =
+            seq {
+               for y in [ 1..h ] do
+                  for x in [ 1..w ] do
+                     yield (Row y, Column x), Number 0 |> Covered
+            }
+            |> Map.ofSeq
+
+         let withBombs =
+            let setBomb x =
+               match x with
+               | Some _ -> Bomb |> Some 
+               | None -> None
+
+            let folder state pos = state |> Map.change pos setBomb
+            (init, bombsPos) ||> fold folder
+
+         Playing(withBombs)
+      | _ -> this
 
 
-let cell = Covered Bomb
+let minefield2 =
+   seq {
+      (Row 1, Column 1), Number 3
+      (Row 1, Column 2), Bomb
+      (Row 2, Column 1), Number 1
+      (Row 2, Column 2), Number 2
+   }
+   |> Map.ofSeq
+   |> Playing
 
-let minefield =
-  seq {
-    yield (1, 1), Number 3
-    yield (0, 0), Bomb
-    yield (1, 0), Number 1
-    yield (0, 1), Number 2
-  }
-  |> Map.ofSeq
-  |> Playing
+let minefield = Setup(Width 3, Height 2, BombsPos [(Row 1, Column 1)])
 
-printfn $"{minefield}"
+printfn $"{minefield.Start}"
